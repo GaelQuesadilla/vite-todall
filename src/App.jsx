b7657fb1510f5dc69caab92f472bcb7ed4c62f01
Gael González <gaeldev032@gmail.com>
2023-08-30 19:11:07 -0700

    Adding constants for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Layout } from "./layout";
 import { Index } from "./routes";
+import { ROUTES } from "./constants/ROUTES";
 export const App = () => {
   return (
     <>
       <Router>
         <Layout>
           <Routes>
-            <Route exact path="*" Component={() => <Index />} />
+            <Route exact path={ROUTES.INDEX.href} Component={() => <Index />} />
           </Routes>
         </Layout>
       </Router>
